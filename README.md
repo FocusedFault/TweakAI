1d0a89cc405c78452a187159158041b19ee44e18
Nux <138419122+FocusedFault@users.noreply.github.com>
2023-11-20 12:24:47 -0500

    1.1.0

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Tweak AI
 
-Makes default AI state "Wander" and makes this state better by adding target locations instead of a random nearby node (original functionality). The state will try for a random player location, then a random player spawn point, then a random map node if the others fail.
-
-Note: This is not an AI rewrite or anything, the AI is still funky this mod just makes initial spawns likelier to spot you and go into their "Combat" state.
+This is not an AI rewrite or anything, the AI is still funky this mod just makes initial enemies likelier to navigate towards players.
 
 ![Wandering Xi Construct](https://i.imgur.com/hy5weGd.gif)
 
 ## Changelog
+
+**1.1.0**
+
+- Internal changes for better results
 
 **1.0.2**
 
--- TweakAI/TweakAI.cs
+++ TweakAI/TweakAI.cs
@@ -1,30 +1,19 @@
 using BepInEx;
 using RoR2;
-using RoR2.Navigation;
 using EntityStates.AI.Walker;
-using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace TweakAI
 {
-  [BepInPlugin("com.Nuxlar.TweakAI", "TweakAI", "1.0.2")]
+  [BepInPlugin("com.Nuxlar.TweakAI", "TweakAI", "1.1.0")]
 
   public class TweakAI : BaseUnityPlugin
   {
-    private List<GameObject> spawnPoints = new();
     public void Awake()
     {
-      On.RoR2.Stage.Start += Stage_Start;
       On.EntityStates.AI.Walker.LookBusy.OnEnter += Busynt;
       On.EntityStates.AI.Walker.Wander.OnEnter += BetterWander;
-    }
-
-    private void Stage_Start(On.RoR2.Stage.orig_Start orig, Stage self)
-    {
-      spawnPoints.Clear();
-      spawnPoints = GameObject.FindObjectsOfType<GameObject>().Where(x => x.name == "SpawnPoint(Clone)").ToList();
-      orig(self);
     }
 
     private void Busynt(On.EntityStates.AI.Walker.LookBusy.orig_OnEnter orig, LookBusy self)
@@ -37,38 +26,25 @@ namespace TweakAI
     private void BetterWander(On.EntityStates.AI.Walker.Wander.orig_OnEnter orig, Wander self)
     {
       orig(self);
-      if (!(bool)(Object)self.ai || !(bool)(Object)self.body)
+      if (!(bool)self.ai || !(bool)self.body)
         return;
-      if (self.body.name == "LunarWispBody(Clone)")
+      if (self.body.master && self.body.master.currentLifeStopwatch > 0 && self.body.name == "LunarWispBody(Clone)")
         return;
-      if ((bool)(Object)PlayerCharacterMasterController.instances[Random.Range(0, PlayerCharacterMasterController.instances.Count - 1)].body)
-        self.targetPosition = PlayerCharacterMasterController.instances[0].body.footPosition;
-      else if (spawnPoints.Count() > 0)
-        self.targetPosition = spawnPoints[Random.Range(0, spawnPoints.Count() - 1)].transform.position;
-      else
+      List<CharacterBody> playerBodies = new();
+      foreach (CharacterMaster cm in UnityEngine.Object.FindObjectsOfType<CharacterMaster>())
       {
-        NodeGraph nodes = self.body.isFlying ? SceneInfo.instance.airNodes : SceneInfo.instance.groundNodes;
-        HullMask hullMask = HullMask.None;
-        switch (self.body.hullClassification)
+        if (cm.teamIndex == TeamIndex.Player)
         {
-          case HullClassification.Human:
-            hullMask = HullMask.Human;
-            break;
-          case HullClassification.Golem:
-            hullMask = HullMask.Golem;
-            break;
-          case HullClassification.BeetleQueen:
-            hullMask = HullMask.BeetleQueen;
-            break;
+          CharacterBody cb = cm.GetBody();
+          if (cb && cb.isPlayerControlled)
+            playerBodies.Add(cb);
         }
-        List<NodeGraph.NodeIndex> withFlagConditions = nodes.GetActiveNodesForHullMaskWithFlagConditions(hullMask, NodeFlags.None, NodeFlags.NoCharacterSpawn);
-        NodeGraph.NodeIndex nodeIndex = withFlagConditions[Random.Range(0, withFlagConditions.Count)];
-        Vector3 position;
-        nodes.GetNodePosition(nodeIndex, out position);
-        self.targetPosition = position;
       }
-      self.ai.SetGoalPosition(self.targetPosition);
-      self.PickNewTargetLookPosition();
+      Transform spawnPoint = Stage.instance.GetPlayerSpawnTransform();
+      if (playerBodies.Count > 0)
+        self.targetPosition = playerBodies[Random.Range(0, playerBodies.Count)].footPosition;
+      else if (spawnPoint)
+        self.targetPosition = spawnPoint.position;
     }
   }
 }
